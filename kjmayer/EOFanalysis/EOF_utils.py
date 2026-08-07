87a3321701747c936fe62316d03ac9f2b6018e95
“kjmayer” <kjmayer@ucar.edu>
2026-08-07 12:17:27 -0600

    looking at EOFs

--- kjmayer/EOFanalysis/EOF_utils.py
+++ kjmayer/EOFanalysis/EOF_utils.py
@@ -103,32 +103,32 @@ def load_subsetdata(
 
 
 
-def load_subsetdata(lat, lon, lev, lat_of_interest, lon_of_interest, lev_of_interest):
-    # Find associated index value for getting data
-    lat_of_interest_near = lat.sel(lat=lat_of_interest, method='nearest')
-    lon_of_interest_near = lon.sel(lon=lon_of_interest, method='nearest')
-    lev_of_interest_near = lev.sel(lev=lev_of_interest, method='nearest')
+# def load_subsetdata(lat, lon, lev, lat_of_interest, lon_of_interest, lev_of_interest):
+#     # Find associated index value for getting data
+#     lat_of_interest_near = lat.sel(lat=lat_of_interest, method='nearest')
+#     lon_of_interest_near = lon.sel(lon=lon_of_interest, method='nearest')
+#     lev_of_interest_near = lev.sel(lev=lev_of_interest, method='nearest')
     
-    ilat_of_interest = np.where(lat.values == lat_of_interest_near.values)[0][0]
-    ilon_of_interest = np.where(lon.values == lon_of_interest_near.values)[0][0]
-    ilev_of_interest = np.where(lev.values == lev_of_interest_near.values)[0][0]
+#     ilat_of_interest = np.where(lat.values == lat_of_interest_near.values)[0][0]
+#     ilon_of_interest = np.where(lon.values == lon_of_interest_near.values)[0][0]
+#     ilev_of_interest = np.where(lev.values == lev_of_interest_near.values)[0][0]
     
-    ilat_of_interest_str = f"{ilat_of_interest:05}"
-    ilon_of_interest_str = f"{ilon_of_interest:05}"
-    ilev_of_interest_str = f"{ilev_of_interest:05}"
+#     ilat_of_interest_str = f"{ilat_of_interest:05}"
+#     ilon_of_interest_str = f"{ilon_of_interest:05}"
+#     ilev_of_interest_str = f"{ilev_of_interest:05}"
 
-    # grab all dates for the U level, lat, and lon point
-    dir_o = "/glade/derecho/scratch/kjmayer/CUVACAR_xai/IG/"
-    files = sorted(
-        glob.glob(dir_o+"*/tensor_steps24_parrallel_lev"+ilev_of_interest_str+"_lat"+ilat_of_interest_str+"_lon"+ilon_of_interest_str+".npy")
-    )
-    files_JJA = files[151:243]
-    files_DJF = np.delete(files, np.s_[59:334], axis=0) #remove non-DJF
+#     # grab all dates for the U level, lat, and lon point
+#     dir_o = "/glade/derecho/scratch/kjmayer/CUVACAR_xai/IG/"
+#     files = sorted(
+#         glob.glob(dir_o+"*/tensor_steps24_parrallel_lev"+ilev_of_interest_str+"_lat"+ilat_of_interest_str+"_lon"+ilon_of_interest_str+".npy")
+#     )
+#     files_JJA = files[151:243]
+#     files_DJF = np.delete(files, np.s_[59:334], axis=0) #remove non-DJF
     
-    IG_allvars_JJA = np.stack([np.load(f).squeeze() for f in files_JJA])
-    IG_allvars_DJF = np.stack([np.load(f).squeeze() for f in files_DJF])
+#     IG_allvars_JJA = np.stack([np.load(f).squeeze() for f in files_JJA])
+#     IG_allvars_DJF = np.stack([np.load(f).squeeze() for f in files_DJF])
 
-    return IG_allvars_JJA, IG_allvars_DJF, ilat_of_interest, ilon_of_interest
+#     return IG_allvars_JJA, IG_allvars_DJF, ilat_of_interest, ilon_of_interest
 
 
 def spatial_cube_subset(
